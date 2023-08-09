b63f6304a389673a12f15b476a08857fc1880f58
Binyameen <bmghafoor@gmail.com>
2023-08-09 12:24:19 -0400

    two array obj

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,15 @@
-// add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(arr1, arr2) {
+  const obj = {};
+  for (let i = 0; i < arr1.length; i++) {
+    let key;
+    if (!arr2[i]) {
+      key = null;
+    } else {
+      key = arr2[i];
+    }
+    obj[arr1[i]] = key;
+  }
+  return obj;
 }
+
+module.exports = twoArrayObject;
