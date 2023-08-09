c5fcdb8b14ed23ef484a871010b8ccf6582a3c61
Binyameen <bmghafoor@gmail.com>
2023-08-09 12:31:46 -0400

    same frequency

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,24 @@
-// add whatever parameters you deem necessary
-function sameFrequency() {
+function sumDigits(integer) {
+  // Convert the integer to a string
+  const str = Math.abs(integer).toString();
+
+  // Initialize a variable to store the sum
+  let sum = 0;
+
+  // Iterate through each character (digit) in the string
+  for (let i = 0; i < str.length; i++) {
+    // Parse the character as an integer and add it to the sum
+    sum += parseInt(str[i]);
+  }
+
+  return sum;
 }
+function sameFrequency(num1, num2) {
+  if (sumDigits(num1) === sumDigits(num2)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+module.exports = sameFrequency;
