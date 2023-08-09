309b7e8508c82b4a02ddb09f6b46bd54d9f0d508
Binyameen <bmghafoor@gmail.com>
2023-08-09 12:14:55 -0400

    average pair

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,18 @@
-// add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(arr, target) {
+  if (arr.length === 0) return false;
+  let leftIdx = 0;
+  let rightIdx = arr.length - 1;
+  while (leftIdx < rightIdx) {
+    const average = (arr[leftIdx] + arr[rightIdx]) / 2;
+    if (average === target) {
+      return true;
+    } else if (average > target) {
+      rightIdx--;
+    } else {
+      leftIdx++;
+    }
+  }
+  return false;
 }
+
+module.exports = averagePair;
