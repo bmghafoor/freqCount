57cb23614595c584ea2cc13449b141c6206c87bd
Binyameen <bmghafoor@gmail.com>
2023-08-09 16:32:50 -0400

    seperate positive

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,15 @@
-// add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(array) {
+  const len = array.length;
+  let negCount = 0;
+
+  for (let i = 0; i < len - negCount; i++) {
+    if (array[i] < 0) {
+      array.push(array.splice(i, 1)[0]);
+      i--;
+      negCount++;
+    }
+  }
+  return array;
 }
+
+module.exports = separatePositive;
