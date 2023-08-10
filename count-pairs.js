988e5febdfa3f14d1bdacba8b91d4abc93de664a
Binyameen <bmghafoor@gmail.com>
2023-08-09 22:24:51 -0400

    count pairs

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,26 @@
-// add whatever parameters you deem necessary
-function countPairs() {
+// create a function to sort an array
+function sortArray(arr) {
+  return arr.slice().sort((a, b) => a - b);
 }
+
+function countPairs(arr, target) {
+  const sortedArr = sortArray(arr);
+  let leftIdx = 0;
+  let rightIdx = sortedArr.length - 1;
+  let count = 0;
+  while (leftIdx < rightIdx) {
+    const sum = sortedArr[leftIdx] + sortedArr[rightIdx];
+    if (sum === target) {
+      count++;
+      leftIdx++;
+      rightIdx--;
+    } else if (sum > target) {
+      rightIdx--;
+    } else {
+      leftIdx++;
+    }
+  }
+  return count;
+}
+
+module.exports = countPairs;
